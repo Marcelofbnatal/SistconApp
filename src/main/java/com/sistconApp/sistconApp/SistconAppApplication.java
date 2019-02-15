58f95ab156bdf0208418b8fe65cc8bbcbfd13760
User <User@192.168.0.6>
2019-02-14 23:32:07 -0200

    Criacao do prjeto

---
+++ src/main/java/com/sistconApp/sistconApp/SistconAppApplication.java
@@ -0,0 +1,14 @@
+package com.sistconApp.sistconApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistconAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistconAppApplication.class, args);
+	}
+
+}
+
